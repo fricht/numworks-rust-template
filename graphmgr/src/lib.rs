fe88ffdcafee1aef3ace07b903cdc2bd5baf30bc
fricht <fricht@users.noreply.github.com>
2025-04-20 16:38:57 +0200

    Added state manager, chnged main program to showcase state manager, increased default RAM (×64).

---
+++ graphmgr/src/lib.rs
@@ -0,0 +1,99 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::{boxed::Box, vec::Vec};
+use libnw::keyboard::KeyboardTimedState;
+
+/// Represents an action to apply on the states stack.
+///
+/// `M` is a pop message (transferred to the state below).
+///
+/// Should only be used with `StateManager`.
+pub enum StackAction<M> {
+    Pop(M),
+    Push(Box<dyn State<M>>),
+    Nop,
+}
+
+/// The state manager.
+///
+/// # Example :
+/// (too basic, todo : make it better)
+/// ```
+/// let mut state_mgr = StateManager::<()>::new();
+/// state_mgr.run(Box::new(MyState::new()));
+/// ```
+pub struct StateManager<M> {
+    stack: Vec<Box<dyn State<M>>>,
+}
+
+impl<M> StateManager<M> {
+    /// Creates a new instance.
+    pub fn new() -> Self {
+        Self { stack: Vec::new() }
+    }
+
+    // should i make these methods public ???
+
+    fn push_to_stack(&mut self, mut state: Box<dyn State<M>>) {
+        if let Some(f) = self.stack.last_mut() {
+            f.pause();
+        }
+        state.create();
+        self.stack.push(state);
+    }
+
+    fn pop_from_stack(&mut self, msg: M) -> Option<Box<dyn State<M>>> {
+        let mut poped_frame = self.stack.pop();
+        match &mut poped_frame {
+            Some(f) => f.quit(),
+            None => return None,
+        }
+        if let Some(f) = self.stack.last_mut() {
+            f.resume(msg);
+        }
+        poped_frame
+    }
+
+    // -----
+
+    /// Here we go !!!\
+    /// (with initial state)
+    pub fn run(&mut self, initial_state: Box<dyn State<M>>) {
+        let mut kb_handler = KeyboardTimedState::new();
+        self.push_to_stack(initial_state);
+        while let Some(frame) = self.stack.last_mut() {
+            kb_handler.fetch();
+            match frame.update(&kb_handler) {
+                StackAction::Pop(msg) => {
+                    self.pop_from_stack(msg);
+                    continue;
+                }
+                StackAction::Push(frame) => {
+                    self.push_to_stack(frame);
+                    continue;
+                }
+                StackAction::Nop => (),
+            }
+            frame.render();
+        }
+    }
+}
+
+pub trait State<M> {
+    /// called when adding state to stack
+    fn create(&mut self) {}
+    /// called when another state is pushed on top
+    fn pause(&mut self) {}
+    /// called when state on top is poped
+    fn resume(&mut self, _pop_message: M) {}
+    /// called when state is poped
+    fn quit(&mut self) {}
+    /// when frame is active, called frequently\
+    /// intended for logic update
+    fn update(&mut self, keyboard_state: &KeyboardTimedState) -> StackAction<M>;
+    /// when frame is active called frequently\
+    /// intended for render logic
+    fn render(&mut self);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 #![no_std]
 
-use libnw::display::{self, CHAR_HEIGHT, Color, LARGE_CHAR_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH};
+extern crate alloc;
+
+use alloc::{
+    boxed::Box,
+    format,
+    string::{String, ToString},
+};
+use graphmgr::{StackAction, State, StateManager};
+use libnw::{
+    display::{self, CHAR_HEIGHT, Color, LARGE_CHAR_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH},
+    keyboard::{KeyboardTimedState, RawKey},
+};
 
 /// How to handle the end of the main app.
 pub enum ExitBehaviour {
@@ -9,17 +20,42 @@ pub enum ExitBehaviour {
     Restart,
 }
 
+struct MainState {
+    pub msg: String,
+}
+
+impl State<()> for MainState {
+    fn update(&mut self, keyboard_state: &KeyboardTimedState) -> StackAction<()> {
+        if keyboard_state.is_key_just_pressed(RawKey::Ok) {
+            StackAction::Push(Box::new(MainState {
+                msg: format!("+{}", self.msg),
+            }))
+        } else if keyboard_state.is_key_just_pressed(RawKey::Back) {
+            StackAction::Pop(())
+        } else {
+            StackAction::Nop
+        }
+    }
+
+    fn render(&mut self) {
+        display::eadk::wait_for_vblank();
+        display::clear_screen(Color::GREEN);
+        display::draw_string(
+            &self.msg,
+            (SCREEN_WIDTH - (self.msg.len() as u16 * LARGE_CHAR_WIDTH)) / 2,
+            (SCREEN_HEIGHT - CHAR_HEIGHT) / 2,
+            true,
+            Color::BLACK,
+            Color::WHITE,
+        );
+    }
+}
+
 /// The core of the application logic
 pub fn main() -> ExitBehaviour {
-    const MESSAGE: &str = "Hey !!";
-    display::clear_screen(Color::GREEN);
-    display::draw_string(
-        MESSAGE,
-        (SCREEN_WIDTH - (MESSAGE.len() as u16 * LARGE_CHAR_WIDTH)) / 2,
-        (SCREEN_HEIGHT - CHAR_HEIGHT) / 2,
-        true,
-        Color::BLACK,
-        Color::WHITE,
-    );
-    ExitBehaviour::Hang
+    let mut state_mgr = StateManager::<()>::new();
+    state_mgr.run(Box::new(MainState {
+        msg: "Hey !!".to_string(),
+    }));
+    ExitBehaviour::Exit
 }
